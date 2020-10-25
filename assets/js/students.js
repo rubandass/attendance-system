ec1e9dedf17b923c7b38aecf8384ace6e1fa8f81
rubandass jhondass <jhonr1@student.op.ac.nz>
2020-10-26 11:47:13 +1300

    Filter stream, Marking students - done

--- assets/js/students.js
+++ assets/js/students.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
     console.log("==============");
     let students = [];
-    let url = '';
+    let filteredStudents = [];
+    let presentCount = 0;
+    let absentCount = 0;
+    let allSelected = false;
+
+    // let url = '';
 
     // fetch('https://api.github.com/gists/b40fa9bba517ff258da395c79edd2477')
     //     .then(response => response.json())
@@ -16,17 +21,34 @@ $(document).ready(function () {
             data.forEach(student => {
                 students.push(student);
             });
+            filteredStudents = students;
             addStream();
             populateStudents(students);
-            // selectStudent();
-            // markingStudent();
             filterStudents();
+            selectAllStudents();
         });
 
+
+    // Select all rows
+    function selectAllStudents() {
+        $('#select-all').click(function () {
+            if ($('#select-all').attr('class').includes('select-all')) {
+                $('#select-all').removeClass('select-all');
+                $('.student').addClass('highlight');
+                allSelected = true;
+            } else {
+                $('#select-all').addClass('select-all');
+                $('.student').removeClass('highlight');
+                allSelected = false;
+            }
+            clearStudentDetails();
+        })
+    }
 
     // adding stream to students
     function addStream() {
         students.forEach((student, index) => {
+            student.marking = 'n';  // add new key:value pair to store marking attendance. By default it is 'n' => 'no marking'
             if (index % 2 == 0) {
                 student.stream = 'stream_a';
             } else {
@@ -39,7 +61,19 @@ $(document).ready(function () {
         let studentsList = '';
         filteredStudents.forEach((student, index) => {
             let attendanceHistory = '';
-            let attendanceMarking = '<button type="button" class="btn btn-sm presentMark present-default">&#10004;</button>' + ' ' + '<button type="button" class="btn btn-sm absentMark absent-default">&#x2718;</button>';
+            let markingPresentClass = '';
+            let markingAbsentClass = '';
+            if (student.marking === 'p') {
+                markingPresentClass = 'present';
+            } else {
+                markingPresentClass = 'marking-default';
+            }
+            if (student.marking === 'a') {
+                markingAbsentClass = 'absent';
+            } else {
+                markingAbsentClass = 'marking-default';
+            }
+            let attendanceMarking = '<button type="button" class="btn btn-sm tickMark ' + markingPresentClass + '">&#10004;</button>' + ' ' + '<button type="button" class="btn btn-sm crossMark ' + markingAbsentClass + '">&#x2718;</button>';
             student.attendance.forEach(marking => {
                 attendanceHistory += '<button type="button" class="btn btn-sm">' + marking + '</button>';
             });
@@ -48,7 +82,7 @@ $(document).ready(function () {
                 + '<td>' + attendanceHistory + '</td>'
                 + '</tr>';
         });
-
+        $('#select-all').addClass('select-all');
         $("#students-list").html(studentsList);
         selectStudent();
         markingStudent();
@@ -56,13 +90,73 @@ $(document).ready(function () {
 
     // Marking student attendance
     function markingStudent() {
-        $('.presentMark').click(function () {
-            $(this).toggleClass('present');
-            console.log($(this).closest('tr'));
+        $('.tickMark').click(function () {
+            if (allSelected) {
+                // Read all table rows if user selects 'Select All'
+                $('#students-list').find('tr').each(function () {
+                    let querySelector = $(this).find('.tickMark');
+                    markingPresent(querySelector, $(this));
+                });
+            } else {
+                let querySelector = $(this);
+                markingPresent(querySelector, $(this).closest('tr'));
+            }
+            allSelected = false;
         });
-        $('.absentMark').click(function () {
-            $(this).toggleClass('absent');
+
+        $('.crossMark').click(function () {
+            if (allSelected) {
+                // Read all table rows if user selects 'Select All'
+                $('#students-list').find('tr').each(function () {
+                    let querySelector = $(this).find('.crossMark');
+                    markingAbsent(querySelector, $(this));
+                });
+            } else {
+                let querySelector = $(this);
+                markingAbsent(querySelector, $(this).closest('tr'));
+            }
+            allSelected = false;
         });
+    }
+
+    // Marking Present
+    function markingPresent(querySelector, thisElement) {
+        querySelector.toggleClass('marking-default present');
+        let activeStudentId = thisElement.data('id');
+        let activeStudent = students.find(x => x.id == activeStudentId);    // Find the element from students array
+        // activeStudent.attendance.push('p');
+        if (activeStudent.marking === 'p') {
+            activeStudent.marking = 'n';
+            $('#present-count').text(--presentCount);
+        } else {
+            activeStudent.marking = 'p';
+            $('#present-count').text(++presentCount);
+            let absentClassName = querySelector.siblings().attr('class');
+            if (absentCount > 0 && absentClassName.includes('absent')) {
+                $('#absent-count').text(--absentCount);
+            }
+            querySelector.siblings().removeClass('absent').addClass('marking-default');
+        }
+    }
+
+    // Marking absent
+    function markingAbsent(querySelector, thisElement) {
+        querySelector.toggleClass('marking-default absent');
+        let activeStudentId = thisElement.closest('tr').data('id');
+        let activeStudent = students.find(x => x.id == activeStudentId);    // Find the element from students array
+        // activeStudent.attendance.push('a');
+        if (activeStudent.marking === 'a') {
+            activeStudent.marking = 'n';
+            $('#absent-count').text(--absentCount);
+        } else {
+            activeStudent.marking = 'a';
+            $('#absent-count').text(++absentCount);
+            let presentClassName = querySelector.siblings().attr('class');
+            if (presentCount > 0 && presentClassName.includes('present')) {
+                $('#present-count').text(--presentCount);
+            }
+            querySelector.siblings().removeClass('present').addClass('marking-default');
+        }
     }
     // Clicking a student row in students list table
     function selectStudent() {
@@ -72,6 +166,7 @@ $(document).ready(function () {
             let activeStudentId = $(this).data('id');
             let activeStudent = students.filter(x => x.id == activeStudentId);    // Filtering the element from students array
             studentDetails(activeStudent);
+            $('#select-all').addClass('select-all');
         });
     }
 
@@ -97,7 +192,7 @@ $(document).ready(function () {
     // filter students based on stream selection
     function filterStudents() {
         $('#stream').change(function () {
-            let filteredStudents = [];
+            filteredStudents = [];
             students.forEach(student => {
                 if (this.value === 'all') {
                     filteredStudents.push(student);
